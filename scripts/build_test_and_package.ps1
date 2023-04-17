05aff28cbcf0a5bd43b11f994f2618bfcaa0146e
Ishansh Lal <lalishansh@gmail.com>
2023-04-17 17:28:28 +0530

    finalized i guess

--- scripts/build_test_and_package.ps1
+++ scripts/build_test_and_package.ps1
@@ -1,26 +1,28 @@
 param(
-	[string][Parameter(Mandatory)]$os,
-	[string][Parameter(Mandatory)]$build_type
+	[string][Parameter(Mandatory)][ValidateSet("Windows", "Linux")]$OS,
+	[string][Parameter(Mandatory)][ValidateSet("Debug", "Release")]$BuildType
 )
 
-if (-not $os) {
-	$os = "windows-latest"
+$env:VCPKG_ROOT = $env:VCPKG_ROOT -replace '\\', '/'
+
+$presetName = "$OS x64 - Ninja - Clang @ $BuildType"
+
+$everything_cool = $false
+try {
+	cmake --preset $presetName
+	pushd "./build/$presetName"
+	
+	cmake --build . --config $build_type
+	cmake --build . --target test
+	
+	cpack -C Release
+	
+	$everything_cool = $true
+} catch { Write-Host $_ }
+finally { popd }
+
+if ($everything_cool) {
+	Remove-Item -Path "./installers/_CPack_Packages" -Recurse -Force -ErrorAction Ignore
 }
 
-if (-not $build_type) {
-	$build_type = "Debug"
-}
-
-$env:VCPKG_ROOT = "$env:VCPKG_ROOT" -replace '\\', '/'
-
-Write-Host "Configuring project for OS: $os and Build Type: $build_type"
-cmake -B build -S . -DCMAKE_BUILD_TYPE=$build_type
-
-Write-Host "Building project"
-cmake --build build
-
-Write-Host "Testing project"
-cmake --build build --target test
-
-Write-Host "Packaging project"
-cmake --install build --prefix install --component Runtime --strip --verbose
+return "./installers/"
